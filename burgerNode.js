4aeca725f25f808d6801effe1d67318a7d1de225
24thsaint <admin@blackout.biz.tm>
2018-05-10 10:38:30 +0800

    Create new block implementation

--- burgerNode.js
+++ burgerNode.js
@@ -1,5 +1,24 @@
 class BurgerNode {
-  constructor() {
-    this.chain = [];
-  }
+    constructor() {
+        this.chain = []
+        this.pendingTransactions = []
+
+        this.createNewBlock('00000000000', '0') // genesisBlock
+
+    }
+
+    createNewBlock(proof, previousHash, confirmedTransactions = []) {
+        const block = {
+            index: this.chain.length + 1,
+            timestamp: new Date().toISOString(),
+            transactions: confirmedTransactions,
+            proof,
+            previousHash
+        }
+
+        this.chain.push(block)
+        return block
+    }
 }
+
+module.exports = BurgerNode
