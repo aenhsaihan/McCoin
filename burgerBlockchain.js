71be290e52b865c5f17a54d8c59ad124f188b5e9
24thsaint <admin@blackout.biz.tm>
2018-05-11 15:37:23 +0800

    feat(Server): Initial implementation of /blocks
    #19

--- burgerBlockchain.js
+++ burgerBlockchain.js
@@ -1,30 +1,32 @@
 const BurgerBlock = require('./burgerBlock');
 
 class BurgerBlockchain {
-  constructor(pendingTransactions, currentDifficulty = 5) {
-    this.blocks = [createGenesisBlock()];
-    this.pendingTransactions = pendingTransactions;
-    this.currentDifficulty = currentDifficulty;
+    constructor(pendingTransactions, currentDifficulty = 5) {
+        this.blocks = [this.createGenesisBlock()];
+        this.pendingTransactions = pendingTransactions;
+        this.currentDifficulty = currentDifficulty;
 
-    this.miningJobs = new Map();
-  }
+        this.miningJobs = new Map();
+    }
 
-  createGenesisBlock() {
-    const genesisBlock = BurgerBlock(
-      0,
-      0,
-      0,
-      '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7',
-      0
-    );
-    return genesisBlock;
-  }
+    createGenesisBlock() {
+        const genesisBlock = new BurgerBlock(
+            0,
+            0,
+            0,
+            '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7',
+            0
+        );
+        return genesisBlock;
+    }
 
-  addBlock(block) {
-    this.blocks.push(block);
-  }
+    addBlock(block) {
+        this.blocks.push(block);
+    }
 
-  createMiningJob(block) {
-    this.miningJobs.set(block.blockDataHash, block);
-  }
+    createMiningJob(block) {
+        this.miningJobs.set(block.blockDataHash, block);
+    }
 }
+
+module.exports = BurgerBlockchain
