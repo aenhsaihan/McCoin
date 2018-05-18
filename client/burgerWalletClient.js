0d5facc4c5c4be3a8528cb72aaea43df67f3583d
Kim Benedict Gulmatico <gulmaticokim@yahoo.com>
2018-05-18 16:45:51 +0800

    Console added

--- client/burgerWalletClient.js
+++ client/burgerWalletClient.js
@@ -1,14 +1,32 @@
 const BurgerWallet = require('../burgerWallet');
-const burgerWallet = new BurgerWallet();
+const readline = require('readline-sync');
+let burgerWallet;
 
 const transaction = {
     "from": "c3293572dbe6ebc60de4a20ed0e21446cae66b17",
     "to": "f51362b7351ef62253a227a77751ad9b2302f911",
     "value": 250123,
     "fee": 10,
-    "dateCreated": "2018-01-10T17:53:48.972Z",
     "data": "KIM",
-    "senderPubKey": burgerWallet.publicKey
 }
 
-burgerWallet.send(transaction);
+var privateKey = readline.question("Enter private key [leave blank to generate new wallet]:");
+
+burgerWallet = privateKey?new BurgerWallet(privateKey):new BurgerWallet();
+console.log(burgerWallet);
+
+let keys =Object.keys( transaction );
+
+console.log("\n====================Enter transaction details=====================\n");
+
+for(let i = 0; i < keys.length; i++){
+    var input = readline.question(keys[i].toString()+": ");
+    transaction[keys[i].toString()] = input;
+    
+}
+
+transaction.dateCreated = new Date().toISOString();
+transaction.senderPubKey = burgerWallet.publicKey;
+
+console.log(transaction);
+  
